5066f1ffe656ba8e70ee4eea5db1bd7c6dc14291
PortHubSL <47505778+PortHubSL@users.noreply.github.com>
2019-11-25 16:44:49 +0200

    fix id

--- lab1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ lab1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,6 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -18,17 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ConstraintLayout bgElement = (ConstraintLayout) findViewById(R.id.my);
+        ConstraintLayout bgElement = (ConstraintLayout) findViewById(R.id.myBG);
         bgElement.setBackgroundColor(Color.CYAN);
         myButtonListenerMethod();
     }
 
     public void myButtonListenerMethod() {
-        button = findViewById(R.id.kek);
+        button = findViewById(R.id.myButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ConstraintLayout bgElement = (ConstraintLayout) findViewById(R.id.my);
+                ConstraintLayout bgElement = (ConstraintLayout) findViewById(R.id.myBG);
                 int color = ((ColorDrawable) bgElement.getBackground()).getColor();
                 if (color == Color.RED) {
                     bgElement.setBackgroundColor(Color.BLUE);
